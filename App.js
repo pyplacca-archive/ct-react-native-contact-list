c10b1cc0ce359863453da2791f9e0720f4014182
pyPlacca <dhegreatprosperity10@gmail.com>
2020-11-12 14:23:06 +0000

    Update App.js

--- App.js
+++ App.js
@@ -5,32 +5,31 @@ import ContactsScreen from './assets/screens/Contacts'
 
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <StatusBar style="light" backgroundColor='#44abee'/>
-      <ContactsScreen />
+	return (
+		<View style={styles.container}>
+			<StatusBar style="light" backgroundColor='#44abee'/>
+			<ContactsScreen />
 
-      {/* Signature */}
-      <Text style={{
-        position: 'absolute',
-        alignSelf: 'center',
-        bottom: -270,
-        color: '#fff',
-        backgroundColor: '#000',
-        padding: 10,
-        width: '100%',
-        textAlign: 'center'
-      }}>
-        David Placca - Codetrain Gen 14
-      </Text>
-    </View>
-  );
+			{/* Signature */}
+			<Text style={{
+				color: '#fff',
+				backgroundColor: '#000',
+				padding: 10,
+				width: '100%',
+				textAlign: 'center'
+			}}>
+				David Placca - Codetrain Gen 14
+			</Text>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
 
-  container: {
-    marginTop: 30,
-  },
+	container: {
+		marginTop: 30,
+		flexGrow: 1,
+		justifyContent: 'space-between'
+	},
 
 });
